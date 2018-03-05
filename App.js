6fd49bf18d216d97725a9f18564842e4ff62572a
Henning Bang Halvorsen <henning.b.halvorsen@gmail.com>
2018-03-05 02:07:18 +0100

    Added support for custom fonts and icons

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';  // This import has to be in every component
 import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';  //This is where you import the components from react-native which you want to use (e.g. View, Button, ...)
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+import { Font, AppLoading } from 'expo';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
+import Icon from 'react-native-vector-icons';
 import Speaker from './src/components/speakers/Speaker';  //This is an example of a importing a component we have written.
 import Program from './src/components/program/Program';
 
@@ -22,20 +24,34 @@ let numberThree = '3';
 export default class App extends Component {  // This is where you name the component and export it for use. This also has to be in every component.
 
   state = {
+    fontLoaded: false,
     index: 0,
     routes: [
-      { key: 'first', title: 'Speakers' },
-      { key: 'second', title: 'Program' },
-      { key: 'third', title: 'Info' },
+      { key: 'first', title: 'Speakers', icon: <FontAwesome>{Icons.userCircle}</FontAwesome>},  // Here you decide the title of the tab and the icon
+      { key: 'second', title: 'Program', icon: <FontAwesome>{Icons.calendarO}</FontAwesome>},
+      { key: 'third', title: 'Info', icon: <FontAwesome>{Icons.infoCircle}</FontAwesome>},
     ],
   };
 
+  // This method runs as soon as the component has been loaded
+  async componentDidMount(){
+    // This is where you load custom fonts. You can see a clear pattern.
+    await Expo.Font.loadAsync({
+      RalewayMedium: require('./assets/fonts/Raleway/Raleway-Medium.ttf'),
+      RobotoMono: require('./assets/fonts/Roboto_Mono/RobotoMono-Medium.ttf'),
+      PatuaOne: require('./assets/fonts/Patua_One/PatuaOne-Regular.ttf'),
+      FontAwesome: require('./assets/fonts/FontAwesome.ttf'),
+    }); 
+    this.setState({ fontLoaded: true });
+  }
+  
   _handleIndexChange = index => this.setState({ index });
  
   _renderFooter = props => <TabBar
                              style={styles.tabs} 
                              indicatorStyle={styles.tabIndicatorStyle} 
                              labelStyle={styles.tabLabelStyle}
+                             renderIcon={this._renderIcon(props)}
                              {...props} 
                             />;
  
@@ -46,9 +62,14 @@ export default class App extends Component {  // This is where you name the comp
   });
 
 
+  _renderIcon = (props) => ({ route, index }) => {
+    return <Text>{route.icon}</Text>
+  }
+
   render() {  // Every component needs a render method that returns the JSX you want to display
-
-
+    if (!this.state.fontLoaded) {
+      return <AppLoading />;  // render some progress indicator
+    }
     return (
       <TabViewAnimated
         navigationState={this.state}
@@ -70,6 +91,7 @@ const styles = StyleSheet.create({  // This is the React Native way to style. Th
   },
   tabLabelStyle: {  // Styles for the tabs label text
     color: '#2a2d22',
+    fontFamily: 'PatuaOne',
   },
   tabIndicatorStyle: {  // Styles for the current tab indicator
     backgroundColor: '#018440',  // This sets the indicator color
